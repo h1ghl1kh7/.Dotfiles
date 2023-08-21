71cef6dad41e57e69d8e2fb493f61690fa9b402d
h1ghl1kh7b0b <h1ghl1kh7b0b@gmail.com>
2023-08-22 01:21:10 +0900

    gef install update

---
+++ gef/README.md
@@ -0,0 +1,7 @@
+# GDB
+
+*all files saved in* `/tools/`
+
+```
+./gdb_setting.sh
+```
---
+++ gef/gef_setting.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+
+echo "[*]gdb gef"
+
+installer_path=$PWD
+
+echo "[+] Checking for required dependencies..."
+if command -v git >/dev/null 2>&1 ; then
+    echo "[-] Git found!"
+else
+    echo "[-] Git not found! Aborting..."
+    echo "[-] Please install git and try again."
+fi
+
+if [ -f ~/.gdbinit ] || [ -h ~/.gdbinit ]; then
+    echo "[+] backing up gdbinit file"
+    cp ~/.gdbinit ~/.gdbinit.back_up
+fi
+
+# Install gef
+echo "[+] Download Gef & Gef-extra"
+wget -O ~/.gef.py -q https://gef.blah.cat/py
+git clone https://github.com/hugsy/gef-extras ~/.gef-extras
+
+cd $installer_path
+
+echo "[+] Setting .gdbinit & .gef.rc"
+cp gdbinit ~/.gdbinit
+cp gef.rc ~/.gef.rc
+
+echo "[+] Done"
