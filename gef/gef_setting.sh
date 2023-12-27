ff72d5fd8658e518a1aa1763e5e8608afd38c917
김휘림 <h1ghl1kh7@h1gh-2.local>
2023-12-27 11:21:11 +0900

    update gef_setting.sh

--- gef/gef_setting.sh
+++ gef/gef_setting.sh
@@ -21,7 +21,50 @@ fi
 
 # Install gef
 echo "[+] Download Gef & Gef-extra"
-wget -O ~/.gef.py -q https://gef.blah.cat/py
+
+set -e
+
+curl_found=0
+wget_found=0
+
+# check dependencies
+if [ "$(command -v curl)" ]; then
+	curl_found=1
+elif [ "$(command -v wget)" ]; then
+	wget_found=1
+else
+	echo "Please install cURL or wget and run again"
+	exit 1
+fi
+
+# Backup gdbinit if any
+if [ -f "${HOME}/.gdbinit" ]; then
+    mv "${HOME}/.gdbinit" "${HOME}/.gdbinit.old"
+fi
+
+if [ $wget_found -eq 1 ]; then
+    latest_tag=$(wget -q -O- "https://api.github.com/repos/hugsy/gef/tags" | grep "name" | head -1 | sed -e 's/"name": "\([^"]*\)",/\1/' -e 's/ *//')
+
+    # Get the hash of the commit
+    branch="${latest_tag}"
+    ref=$(wget -q -O- https://api.github.com/repos/hugsy/gef/git/ref/heads/${branch} | grep '"sha"' | tr -s ' ' | cut -d ' ' -f 3 | tr -d "," | tr -d '"')
+
+    # Download the file
+    wget -q "https://github.com/hugsy/gef/raw/${branch}/gef.py" -O "${HOME}/.gef-${ref}.py"
+elif [ $curl_found -eq 1 ]; then
+    latest_tag=$(curl -s "https://api.github.com/repos/hugsy/gef/tags" | grep "name" | head -1 | sed -e 's/"name": "\([^"]*\)",/\1/' -e 's/ *//')
+
+    # Get the hash of the commit
+    branch="${latest_tag}"
+    ref=$(curl --silent https://api.github.com/repos/hugsy/gef/git/ref/heads/${branch} | grep '"sha"' | tr -s ' ' | cut -d ' ' -f 3 | tr -d "," | tr -d '"')
+
+    # Download the file
+    curl --silent --location --output "${HOME}/.gef-${ref}.py" "https://github.com/hugsy/gef/raw/${branch}/gef.py"
+fi
+
+# Create the new gdbinit
+echo "source ~/.gef-${ref}.py" > ~/.gdbinit
+
 git clone https://github.com/hugsy/gef-extras ~/.gef-extras
 
 echo "[+] Download Pwngdb"
@@ -30,7 +73,7 @@ git clone https://github.com/scwuaptx/Pwngdb.git ~/.Pwngdb
 cd $installer_path
 
 echo "[+] Setting .gdbinit & .gef.rc"
-cp gdbinit ~/.gdbinit
+cat gdbinit >> ~/.gdbinit
 cp gef.rc ~/.gef.rc
 
 echo "[+] Done"
